745da7ee49d06457b1c4fca019a45b1e7b073a97
diegogpage <diegogpage2@gmail.com>
2025-02-14 11:48:46 +0100

    AnimacionesBien

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -44,26 +44,27 @@ public class PlayerScript : MonoBehaviour
         if (horizontal != 0f || vertical != 0)
         {
             animator.SetBool("isWalking", true);
+            animator.SetFloat("InputX", horizontal);
+            animator.SetFloat("InputY", vertical);
 
         }
         //limitador de velocidad diagonal
         if (horizontal != 0 && vertical != 0)
         {
-
             horizontal *= moveLimiter;
             vertical *= moveLimiter;
         }
 
-        //la idea es que esto se guarde antes de actualizar la direccion actual,
-        //para que el personaje se quede mirando en la ultima dirección en la que andó,
-        //pero aún no he conseguido hacerlo
-        animator.SetFloat("LastInputX", animator.GetFloat("InputX"));
-        animator.SetFloat("LastInputY", animator.GetFloat("InputY"));
+        ////la idea es que esto se guarde antes de actualizar la direccion actual,
+        ////para que el personaje se quede mirando en la ultima dirección en la que andó,
+        ////pero aún no he conseguido hacerlo
+        //animator.SetFloat("LastInputX", animator.GetFloat("InputX"));
+        //animator.SetFloat("LastInputY", animator.GetFloat("InputY"));
 
-        //velocidad y floats direccionales actuales para el animador
-        rb.velocity = new Vector2(horizontal * moveSpeed, vertical * moveSpeed);
-        animator.SetFloat("InputX", horizontal);
-        animator.SetFloat("InputY", vertical);
+        ////velocidad y floats direccionales actuales para el animador
+        //rb.velocity = new Vector2(horizontal * moveSpeed, vertical * moveSpeed);
+        //animator.SetFloat("InputX", horizontal);
+        //animator.SetFloat("InputY", vertical);
 
         //update de variables de animador cuando se termina el movimiento
         if (horizontal == 0 && vertical == 0)
