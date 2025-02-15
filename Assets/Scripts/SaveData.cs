990f6b99f08e15b5ac1f96a77ab93f85c5064780
diegogpage <diegogpage2@gmail.com>
2025-02-15 23:02:35 +0100

    SistemaGuardado

---
+++ Assets/Scripts/SaveData.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class SaveData
+{
+    //Aquí únicamente vamos a guardar los datos
+    public Vector3 playerPosition;
+    public string mapBoundary; //El nombre del límite del mapa
+
+    //Aquí también guardaríamos el inventario y demás
+}
