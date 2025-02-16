b8c7bc3c5d2be914b1f0447ac0d122df5ed8fb92
glopezy <glopezy1999@gmail.com>
2025-02-16 11:20:58 +0100

    NPC Logic

---
+++ Assets/Scripts/NPCScript.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCScript : MonoBehaviour, IInteractable
+{
+    public bool IsSpeaking { get; private set; }
+
+    [SerializeField] private GameObject dialogue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        IsSpeaking = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool CanInteract()
+    {
+        return true;
+    }
+
+    public void Interact()
+    {
+        if (!IsSpeaking)
+        {
+            Speak();
+        }
+        else
+        {
+            StopSpeaking();
+        }
+
+    }
+
+    private void Speak()
+    {
+        IsSpeaking = true;
+        dialogue.SetActive(true);
+    }
+
+    private void StopSpeaking()
+    {
+        IsSpeaking=false;
+        dialogue.SetActive(false);
+    }
+}
