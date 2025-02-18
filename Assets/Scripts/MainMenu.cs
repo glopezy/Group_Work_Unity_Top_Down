69c50c911645351c4eb00421c60d0e3d74cc4c9c
diegogpage <diegogpage2@gmail.com>
2025-02-18 12:14:19 +0100

    NewGame arreglado

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    private string saveLocation;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        saveLocation = Path.Combine(Application.persistentDataPath, "saveData.json");
     }
 
     // Update is called once per frame
@@ -19,7 +22,17 @@ public class MainMenu : MonoBehaviour
 
     public void NewGame()
     {
-        SceneManager.LoadScene("CutScene1");
+        SaveData newSaveData = new SaveData
+        {
+            playerPosition = Vector3.zero,
+            mapBoundary = "Town1",
+            inventorySaveData = new List<InventorySaveData>(),
+            chestSaveData = new List<ChestSaveData>(),
+        };
+
+        File.WriteAllText(saveLocation, JsonUtility.ToJson(newSaveData));
+
+        SceneManager.LoadScene("InitScene");
     }
 
     public void Continue()
